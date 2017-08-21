4f4d1289ee866808bdc3550e92a24c07267090f5
lsw8168 <lsw8168@naver.com>
2017-08-21 13:31:07 +0900

    basic

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './src/index.html'
+        })
+    ]
+}
